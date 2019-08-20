2ad911b2c014f28ffe0db2cf6fd8e81f8c00df24
lekkla <lekkla.tong@gamil.com>
2019-08-20 20:55:01 +0700

    c

--- index.js
+++ index.js
@@ -69,11 +69,11 @@ app.post('/webhook', (req, res) => {
 
 // Accepts GET requests at the /webhook endpoint
 app.get('/webhook', (req, res) => {
-
+res.send(req.query)
   /** UPDATE YOUR VERIFY TOKEN **/
   const VERIFY_TOKEN = process.env.VERIFICATION_TOKEN;
-console.log(VERIFY_TOKEN,req.query);
-res.send(req.query)
+//console.log(VERIFY_TOKEN,req.query);
+
   // Parse params from the webhook verification request
   let mode = req.query['hub.mode'];
   let token = req.query['hub.verify_token'];
