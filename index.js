a8c8a6c9eaee334206851e0ca3a47f5f11062acc
lekkla <lekkla.tong@gamil.com>
2019-08-21 15:19:15 +0700

    test

--- index.js
+++ index.js
@@ -47,71 +47,23 @@ function sendTextMessage(sender, text) {
         json: {
           "recipient":{
             "id":sender
-          }, 
-          "message": {
-            "attachment": {
-              "type": "template",
-              "payload": {
-                "template_type": "list",
-                "top_element_style": "compact",
-                "elements": [
-                  {
-                    "title": "Classic T-Shirt Collection",
-                    "subtitle": "See all our colors",
-                    "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",          
-                    "buttons": [
-                      {
-                        "title": "View",
-                        "type": "web_url",
-                        "url": "https://peterssendreceiveapp.ngrok.io/collection",
-                        "messenger_extensions": true,
-                        "webview_height_ratio": "tall",
-                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
-                      }
-                    ]
-                  },
-                  {
-                    "title": "Classic White T-Shirt",
-                    "subtitle": "See all our colors",
-                    "default_action": {
-                      "type": "web_url",
-                      "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
-                      "messenger_extensions": false,
-                      "webview_height_ratio": "tall"
-                    }
-                  },
-                  {
-                    "title": "Classic Blue T-Shirt",
-                    "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
-                    "subtitle": "100% Cotton, 200% Comfortable",
-                    "default_action": {
-                      "type": "web_url",
-                      "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
-                      "messenger_extensions": true,
-                      "webview_height_ratio": "tall",
-                      "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-                    },
-                    "buttons": [
-                      {
-                        "title": "Shop Now",
-                        "type": "web_url",
-                        "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
-                        "messenger_extensions": true,
-                        "webview_height_ratio": "tall",
-                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
-                      }
-                    ]        
-                  }
-                ],
-                 "buttons": [
-                  {
-                    "title": "View More",
-                    "type": "postback",
-                    "payload": "payload"            
-                  }
-                ]  
+          },
+          "messaging_type": "RESPONSE",
+          "message":{
+            "text": "Pick a color:",
+            "quick_replies":[
+              {
+                "content_type":"text",
+                "title":"Red",
+                "payload":"<POSTBACK_PAYLOAD>",
+                "image_url":"http://example.com/img/red.png"
+              },{
+                "content_type":"text",
+                "title":"Green",
+                "payload":"<POSTBACK_PAYLOAD>",
+                "image_url":"http://example.com/img/green.png"
               }
-            }
+            ]
           }}
     }, function(error, response, body) {
         if (error) {
