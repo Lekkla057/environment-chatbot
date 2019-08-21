0b5972e296553877326e945c6f854b25faca95a6
lekkla <lekkla.tong@gamil.com>
2019-08-21 14:56:50 +0700

    test

--- index.js
+++ index.js
@@ -45,11 +45,42 @@ function sendTextMessage(sender, text) {
         },
         method: 'POST',
         json: {
-            recipient: {
-                id: sender
-            },
-            message: messageData,
-        }
+          recipient:{
+            id:sender
+          },
+          message:{
+            attachment:{
+              type:"template",
+              payload:{
+                template_type:"generic",
+                elements:[
+                   {
+                    title:"Welcome!",
+                    image_url:"https://petersfancybrownhats.com/company_image.png",
+                    subtitle:"We have the right hat for everyone.",
+                    default_action: {
+                      type: "web_url",
+                      url: "https://petersfancybrownhats.com/view?item=103",
+                      messenger_extensions: false,
+                      webview_height_ratio: "tall",
+                      fallback_url: "https://petersfancybrownhats.com/"
+                    },
+                    buttons:[
+                      {
+                        type:"web_url",
+                        url:"https://petersfancybrownhats.com",
+                        title:"View Website"
+                      },{
+                        type:"postback",
+                        title:"Start Chatting",
+                        payload:"DEVELOPER_DEFINED_PAYLOAD"
+                      }              
+                    ]      
+                  }
+                ]
+              }
+            }
+          }}
     }, function(error, response, body) {
         if (error) {
             console.log('Error:', error);
